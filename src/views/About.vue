855115a1f9f04268cf1a5265e49fd95d73a393f4
Zack Borges <zackartborges@gmail.com>
2021-04-08 11:46:00 -0400

    attempts to get slick carosuel installed

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="slider">
+      $(".slider").slick({ // normal options... infinite: false, // the magic responsive: [{ breakpoint: 1024, settings:
+      { slidesToShow: 3, infinite: true } }, { breakpoint: 600, settings: { slidesToShow: 2, dots: true } }, {
+      breakpoint: 300, settings: "unslick" // destroys slick }] });
+    </div>
   </div>
 </template>
+<script>
+$(document).ready(function(){
+  $('.your-class').slick({
+    setting-name: setting-value
+  });
+});
+</script>
